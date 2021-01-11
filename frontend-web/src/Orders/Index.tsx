92c1cc17f79a85115533052387650e342c058a68
willian <willian@bycode.com.br>
2021-01-10 23:16:52 -0300

    fetching product list

--- frontend-web/src/Orders/Index.tsx
+++ frontend-web/src/Orders/Index.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchProducts } from "../Api";
+import ProductsList from "./ProductsList";
 import StepsHeader from "./StepsHeader";
 import './styles.css';
+import { Product } from "./Types";
  
 function Orders() {
+
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(Error => console.log(Error))
+    }, []);
+
     return (
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products} />
         </div>
         
     )
